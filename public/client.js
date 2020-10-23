a976ae2b0476bc5372e281975c199c65f1e19cd8
Glitch (airtable-example) <none>
2020-10-23 13:49:26 +0000

    🏫🚏 Checkpoint ./public/client.js:20609/979 ./views/index.html:20609/15

--- public/client.js
+++ public/client.js
@@ -2,12 +2,24 @@
 // run by the browser each time your view template is loaded
 
 $(function() {
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-   L.tileLayer('https://.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
+  var userLocation = null;
+  if(!navigator.geolocation) {
+    userLocation = [53.463059, -2.291340];
+  } else {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      userLocation = [position.coords.latitude, position.coords.longitude];
+      console.log(userLocation);
+    });
+  }
+  var mymap = L.map('mapid').setView(userLocation, 13);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
      attribution:
          '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
      maxZoom: 18
- }).addTo(mymap);
+  }).addTo(mymap);
+  
+  /*
   $.getJSON('/data', function(data) {
     var $dataContainer = $('#data-container');
       
@@ -28,9 +40,10 @@ $(function() {
       var notes = record.notes;
       text += "<li><a href=\""+url+"\">"+name+"</a></li>";
       var marker = L.marker([lat, lon]).addTo(mymap);
-      marker.bindPopup("<strong>"+name+"</strong><br>I am a popup.").openPopup();
+      marker.bindPopup("<strong>"+name+"</strong><br>I am a popup.");
     });
     
     $dataContainer.html("<ul>" + text + "</ul>");
   });
+  */
 });

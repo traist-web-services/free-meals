67e2a0e20b1ea1f05d2c6200db722577084f12c7
Glitch (free-meals) <none>
2020-10-23 23:00:11 +0000

    Exporting to traist-web-services/free-meals ./public/client.js:20609/85 ./public/style.css:20609/35

--- public/client.js
+++ public/client.js
@@ -6,10 +6,10 @@ $(function() {
     iconUrl: 'https://cdn.glitch.com/4c0f61c1-ab19-4504-a817-db81aa851c36%2Fmarker-icon.png?v=1603491669838',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
-    popupAnchor: [-3, -76],
+    popupAnchor: [0, -50],
     shadowUrl: 'https://cdn.glitch.com/4c0f61c1-ab19-4504-a817-db81aa851c36%2Fmarker-shadow.png?v=1603491093868',
     shadowSize: [41, 41],
-    shadowAnchor: [21, 41]
+    shadowAnchor: [10, 41]
 })
   
   
